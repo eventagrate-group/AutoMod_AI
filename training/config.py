6cc9527b4a7257051e16983f65d7cd165a6a75c8
Dercio Anselmo <dercio@branchdev.io>
2025-08-22 15:17:22 +0400

    Added nltk.data.path.append('/home/branch/nltk_data') to ensure NLTK uses custom data path

--- training/config.py
+++ training/config.py
@@ -1,6 +1,6 @@
 CONFIG = {
-    'dataset_path': 'train.csv',
+    'dataset_path': '/home/branch/projects/toxictext-scanner/training/train.csv',
     'new_data_path': '/home/branch/Downloads/synthetic_toxic_tweets_new1.csv',
-    'model_path': '../inference/model.joblib',
-    'vectorizer_path': '../inference/vectorizer.joblib'
+    'model_path': '/home/branch/projects/toxictext-scanner/inference/model.joblib',
+    'vectorizer_path': '/home/branch/projects/toxictext-scanner/inference/vectorizer.joblib'
 }
