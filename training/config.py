f8a1b6fdda938d43c9f982db065be83d0cbb8dee
Dercio Anselmo <dercio@branchdev.io>
2025-08-27 16:47:31 +0400

    Small dataset config for traning arabic test

--- training/config.py
+++ training/config.py
@@ -6,7 +6,8 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CONFIG = {
     'dataset_path': os.path.join(PROJECT_ROOT, 'training', 'data', 'train.csv'),
     'new_data_path': os.path.join(PROJECT_ROOT, 'training', 'data', 'new_training_data.csv'),
-    'new_data_path_ar': os.path.join(PROJECT_ROOT, 'training', 'data_arabic', 'new_training_data.csv'),
+    #'new_data_path_ar': os.path.join(PROJECT_ROOT, 'training', 'data_arabic', 'new_training_data.csv'),
+    'new_data_path_ar': os.path.join(PROJECT_ROOT, 'training', 'data_arabic', 'small_training_data.csv'),
     'model_path': os.path.join(PROJECT_ROOT, 'inference', 'model.joblib'),
     'vectorizer_path': os.path.join(PROJECT_ROOT, 'inference', 'vectorizer.joblib'),
     'model_path_ar': os.path.join(PROJECT_ROOT, 'inference', 'model_ar.joblib'),
